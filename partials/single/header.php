3e31d05bd3209788074a1e36a8bb592a4c899698
Nicolas <oceanwptheme@gmail.com>
2017-07-20 18:24:37 +0200

    Add filter to change the heading via child theme

--- partials/single/header.php
+++ partials/single/header.php
@@ -13,8 +13,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Return if quote format
 if ( 'quote' == get_post_format() ) {
 	return;
-} ?>
+}
+
+// Heading
+$heading = apply_filters( 'ocean_single_post_heading', 'h2' ); ?>
 
 <header class="entry-header clr">
-	<h2 class="single-post-title entry-title"<?php oceanwp_schema_markup( 'headline' ); ?>><?php the_title(); ?></h2><!-- .single-post-title -->
+	<<?php echo esc_attr( $heading ); ?> class="single-post-title entry-title"<?php oceanwp_schema_markup( 'headline' ); ?>><?php the_title(); ?></<?php echo esc_attr( $heading ); ?>><!-- .single-post-title -->
 </header><!-- .entry-header -->
