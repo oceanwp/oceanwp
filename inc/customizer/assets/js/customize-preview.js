71d2e8f9d0f219148895f1b7a6119de01b7ebff4
RajeebTheGreat <rajeebthegreat@gmail.com>
2018-05-04 03:35:00 -0400

    Add JS for customizer preview

--- inc/customizer/assets/js/customize-preview.js
+++ inc/customizer/assets/js/customize-preview.js
@@ -124,6 +124,24 @@
 			'arrow',
 			'square'
 		];
+		eddProductsCol 		= [
+			'1',
+			'2',
+			'3',
+			'4'
+		],
+		eddProductsTabletCol = [
+			'tablet-1-col',
+			'tablet-2-col',
+			'tablet-3-col',
+			'tablet-4-col'
+		],
+		eddProductsMobileCol = [
+			'mobile-1-col',
+			'mobile-2-col',
+			'mobile-3-col',
+			'mobile-4-col'
+		],
 
 	/******** WordPress Core *********/
 
@@ -1337,6 +1355,271 @@
 				var $child = $( '.customizer-ocean_woo_product_both_sidebars_sidebars_width' );
 				if ( to ) {
 					var style = '<style class="customizer-ocean_woo_product_both_sidebars_sidebars_width">@media only screen and (min-width: 960px){ body.single-product.content-both-sidebars .widget-area{width:' + to + '%;} body.single-product.content-both-sidebars.scs-style .content-area{left:' + to + '%;} body.single-product.content-both-sidebars.ssc-style .content-area{left:'+ to * 2 +'%;} }</style>';
+					if ( $child.length ) {
+						$child.replaceWith( style );
+					} else {
+						$( 'head' ).append( style );
+					}
+				} else {
+					$child.remove();
+				}
+			} );
+		} );
+
+	/******** EDD *********/
+
+		// Cart style
+		api('ocean_edd_cart_dropdown_style', function( value ) {
+			value.bind( function( newval ) {
+				var cart = $( '.edd-menu-icon' );
+				if ( cart.length ) {
+					$.each( wooCartStyle, function( i, v ) {
+						cart.removeClass( v );
+					});
+					cart.addClass( newval );
+				}
+			});
+		});
+
+		// Cart icon
+		api('ocean_edd_menu_icon', function( value ) {
+			value.bind( function( newval ) {
+				var $cart = $( '#site-navigation-wrap .dropdown-menu > li > a.eddmenucart i' );
+
+				if ( $cart.length ) {
+					$cart.removeClass();
+					$cart.addClass( newval );
+				}
+			});
+		});
+
+		// Custom Cart icon
+		api('ocean_edd_menu_custom_icon', function( value ) {
+			value.bind( function( newval ) {
+				var $cart = $( '#site-navigation-wrap .dropdown-menu > li > a.eddmenucart i' );
+
+				if ( $cart.length ) {
+					$cart.removeClass();
+					$cart.addClass( newval );
+				}
+			});
+		});
+
+		// Menu cart icon size
+		api( 'ocean_edd_menu_icon_size', function( value ) {
+			value.bind( function( to ) {
+				var $child = $( '.customizer-ocean_edd_menu_icon_size' );
+				if ( to ) {
+					var style = '<style class="customizer-ocean_edd_menu_icon_size">.eddmenucart i { font-size: ' + to + 'px; }</style>';
+					if ( $child.length ) {
+						$child.replaceWith( style );
+					} else {
+						$( 'head' ).append( style );
+					}
+				} else {
+					$child.remove();
+				}
+			} );
+		} );
+
+		// Menu cart icon size tablet
+		api( 'ocean_edd_menu_icon_size_tablet', function( value ) {
+			value.bind( function( to ) {
+				var $child = $( '.customizer-ocean_edd_menu_icon_size_tablet' );
+				if ( to ) {
+					var style = '<style class="customizer-ocean_edd_menu_icon_size_tablet">@media (max-width: 768px){.oceanwp-mobile-menu-icon a.eddmenucart { font-size: ' + to + 'px; }}</style>';
+					if ( $child.length ) {
+						$child.replaceWith( style );
+					} else {
+						$( 'head' ).append( style );
+					}
+				} else {
+					$child.remove();
+				}
+			} );
+		} );
+
+		// Menu cart icon size mobile
+		api( 'ocean_edd_menu_icon_size_mobile', function( value ) {
+			value.bind( function( to ) {
+				var $child = $( '.customizer-ocean_edd_menu_icon_size_mobile' );
+				if ( to ) {
+					var style = '<style class="customizer-ocean_edd_menu_icon_size_mobile">@media (max-width: 480px){.oceanwp-mobile-menu-icon a.eddmenucart { font-size: ' + to + 'px; }}</style>';
+					if ( $child.length ) {
+						$child.replaceWith( style );
+					} else {
+						$( 'head' ).append( style );
+					}
+				} else {
+					$child.remove();
+				}
+			} );
+		} );
+
+		// Menu cart icon center vertically
+		api( 'ocean_edd_menu_icon_center_vertically', function( value ) {
+			value.bind( function( to ) {
+				var $child = $( '.customizer-ocean_edd_menu_icon_center_vertically' );
+				if ( to ) {
+					var style = '<style class="customizer-ocean_edd_menu_icon_center_vertically">.eddmenucart i { top: ' + to + 'px; }</style>';
+					if ( $child.length ) {
+						$child.replaceWith( style );
+					} else {
+						$( 'head' ).append( style );
+					}
+				} else {
+					$child.remove();
+				}
+			} );
+		} );
+
+		// Menu cart icon center vertically tablet
+		api( 'ocean_edd_menu_icon_center_vertically_tablet', function( value ) {
+			value.bind( function( to ) {
+				var $child = $( '.customizer-ocean_edd_menu_icon_center_vertically_tablet' );
+				if ( to ) {
+					var style = '<style class="customizer-ocean_edd_menu_icon_center_vertically_tablet">@media (max-width: 768px){.oceanwp-mobile-menu-icon a.eddmenucart { top: ' + to + 'px; }}</style>';
+					if ( $child.length ) {
+						$child.replaceWith( style );
+					} else {
+						$( 'head' ).append( style );
+					}
+				} else {
+					$child.remove();
+				}
+			} );
+		} );
+
+		// Menu cart icon center vertically mobile
+		api( 'ocean_edd_menu_icon_center_vertically_mobile', function( value ) {
+			value.bind( function( to ) {
+				var $child = $( '.customizer-ocean_edd_menu_icon_center_vertically_mobile' );
+				if ( to ) {
+					var style = '<style class="customizer-ocean_edd_menu_icon_center_vertically_mobile">@media (max-width: 480px){.oceanwp-mobile-menu-icon a.eddmenucart { top: ' + to + 'px; }}</style>';
+					if ( $child.length ) {
+						$child.replaceWith( style );
+					} else {
+						$( 'head' ).append( style );
+					}
+				} else {
+					$child.remove();
+				}
+			} );
+		} );
+
+		// Archives products columns
+		api('ocean_edd_archive_columns', function( value ) {
+			value.bind( function( newval ) {
+				var archivesCol = $( '.oceanwp-row .edd_downloads_list' );
+				if ( archivesCol.length ) {
+					$.each( eddProductsCol, function( i, v ) {
+						archivesCol.parent().removeClass( 'archives-col-'+ v );
+						archivesCol.removeClass( 'span_1_of_'+ v );
+					});
+					archivesCol.parent().addClass( 'custom-col archives-col-'+ newval );
+					archivesCol.addClass( 'span_1_of_'+ newval );
+				}
+			});
+		});
+
+		// Archives tablet columns class
+		api('ocean_edd_tablet_archive_columns', function( value ) {
+			value.bind( function( newval ) {
+				var archivesCol = $( '.oceanwp-row .edd_downloads_list' );
+				if ( archivesCol.length ) {
+					$.each( eddProductsTabletCol, function( i, v ) {
+						archivesCol.removeClass( v );
+					});
+					archivesCol.addClass( 'tablet-col' );
+					archivesCol.addClass( 'tablet-'+ newval +'-col' );
+				}
+			});
+		});
+
+		// Archives mobile columns class
+		api('ocean_edd_mobile_archive_columns', function( value ) {
+			value.bind( function( newval ) {
+				var archivesCol = $( '.oceanwp-row .edd_downloads_list' );
+				if ( archivesCol.length ) {
+					$.each( eddProductsMobileCol, function( i, v ) {
+						archivesCol.removeClass( v );
+					});
+					archivesCol.addClass( 'mobile-col' );
+					archivesCol.addClass( 'mobile-'+ newval +'-col' );
+				}
+			});
+		});
+
+		// Content Alignment
+		api('ocean_edd_product_entry_content_alignment', function( value ) {
+			value.bind( function( newval ) {
+				var content_alignment = $( '.oceanwp-row .edd_downloads_list .edd-download' );
+				if ( content_alignment.length ) {
+					$.each( wooContentAlignment, function( i, v ) {
+						content_alignment.removeClass( v );
+					});
+					content_alignment.addClass( 'owp-content-'+ newval );
+				}
+			});
+		});
+
+		// Both Sidebars layout shop page content width
+		api( 'ocean_edd_archive_both_sidebars_content_width', function( value ) {
+			value.bind( function( to ) {
+				var $child = $( '.customizer-ocean_edd_archive_both_sidebars_content_width' );
+				if ( to ) {
+					var style = '<style class="customizer-ocean_edd_archive_both_sidebars_content_width">@media only screen and (min-width: 960px){ body.edd.archive.content-both-sidebars .content-area { width: ' + to + '%; }				body.edd.archive.content-both-sidebars.scs-style .widget-area.sidebar-secondary, 			body.edd.archive.content-both-sidebars.ssc-style .widget-area {left: -' + to + '%;} }</style>';
+					if ( $child.length ) {
+						$child.replaceWith( style );
+					} else {
+						$( 'head' ).append( style );
+					}
+				} else {
+					$child.remove();
+				}
+			} );
+		} );
+
+		// Both Sidebars layout archive page sidebars width
+		api( 'ocean_edd_archive_both_sidebars_sidebars_width', function( value ) {
+			value.bind( function( to ) {
+				var $child = $( '.customizer-ocean_edd_archive_both_sidebars_sidebars_width' );
+				if ( to ) {
+					var style = '<style class="customizer-ocean_edd_archive_both_sidebars_sidebars_width">@media only screen and (min-width: 960px){ body.edd.archive.content-both-sidebars .widget-area{width:' + to + '%;} body.edd.archive.content-both-sidebars.scs-style .content-area{left:' + to + '%;} body.edd.archive.content-both-sidebars.ssc-style .content-area{left:'+ to * 2 +'%;} }</style>';
+					if ( $child.length ) {
+						$child.replaceWith( style );
+					} else {
+						$( 'head' ).append( style );
+					}
+				} else {
+					$child.remove();
+				}
+			} );
+		} );
+
+		// Both Sidebars layout single edd content width
+		api( 'ocean_edd_download_both_sidebars_content_width', function( value ) {
+			value.bind( function( to ) {
+				var $child = $( '.customizer-ocean_edd_download_both_sidebars_content_width' );
+				if ( to ) {
+					var style = '<style class="customizer-ocean_edd_download_both_sidebars_content_width">@media only screen and (min-width: 960px){ body.single-edd.content-both-sidebars .content-area { width: ' + to + '%; } body.single-edd.content-both-sidebars.scs-style .widget-area.sidebar-secondary, body.single-edd.content-both-sidebars.ssc-style .widget-area {left: -' + to + '%;} }</style>';
+					if ( $child.length ) {
+						$child.replaceWith( style );
+					} else {
+						$( 'head' ).append( style );
+					}
+				} else {
+					$child.remove();
+				}
+			} );
+		} );
+
+		// Both Sidebars layout single edd sidebars width
+		api( 'ocean_edd_download_both_sidebars_sidebars_width', function( value ) {
+			value.bind( function( to ) {
+				var $child = $( '.customizer-ocean_edd_download_both_sidebars_sidebars_width' );
+				if ( to ) {
+					var style = '<style class="customizer-ocean_edd_download_both_sidebars_sidebars_width">@media only screen and (min-width: 960px){ body.single-edd.content-both-sidebars .widget-area{width:' + to + '%;} body.single-edd.content-both-sidebars.scs-style .content-area{left:' + to + '%;} body.single-edd.content-both-sidebars.ssc-style .content-area{left:'+ to * 2 +'%;} }</style>';
 					if ( $child.length ) {
 						$child.replaceWith( style );
 					} else {
--- inc/customizer/settings/edd.php
+++ inc/customizer/settings/edd.php
@@ -797,16 +797,16 @@ if ( ! class_exists( 'OceanWP_EDD_Customizer' ) ) :
 			/**
 			 * Product Entry Content Alignment
 			 */
-			$wp_customize->add_setting( 'ocean_edd_edd_entry_content_alignment', array(
+			$wp_customize->add_setting( 'ocean_edd_product_entry_content_alignment', array(
 				'transport' 			=> 'postMessage',
 				'default'           	=> 'center',
 				'sanitize_callback' 	=> 'oceanwp_sanitize_select',
 			) );
 
-			$wp_customize->add_control( new OceanWP_Customizer_Buttonset_Control( $wp_customize, 'ocean_edd_edd_entry_content_alignment', array(
+			$wp_customize->add_control( new OceanWP_Customizer_Buttonset_Control( $wp_customize, 'ocean_edd_product_entry_content_alignment', array(
 				'label'	   				=> esc_html__( 'Content Alignment', 'oceanwp' ),
 				'section'  				=> 'ocean_edd_archives',
-				'settings' 				=> 'ocean_edd_edd_entry_content_alignment',
+				'settings' 				=> 'ocean_edd_product_entry_content_alignment',
 				'priority' 				=> 10,
 				'choices' 				=> array(
 					'left' 		=> esc_html__( 'Left', 'oceanwp' ),
