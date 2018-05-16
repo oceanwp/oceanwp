cf824d749080b677ef04e5aa3cf097e8b9be75dc
Nicolas <oceanwptheme@gmail.com>
2018-05-17 01:39:47 +0200

    Fix - Merge Conflicts

--- assets/js/third/edd/edd-cart-ajax.js
+++ assets/js/third/edd/edd-cart-ajax.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var $j = jQuery.noConflict();
 
 $j( document ).on( 'ready', function() {
@@ -30,37 +29,4 @@ function oceanwpEDDCartDetails() {
 		cartTotalAmount.html( response.total );
 	});
 
-=======
-var $j = jQuery.noConflict();
-
-$j( document ).on( 'ready', function() {
-	"use strict";
-    // Edd display cart
-    oceanwpEDDCartDetails();
-} );
-
-/* ==============================================
-Easy Digital Downloads Cart Details
-============================================== */
-function oceanwpEDDCartDetails() {
-	"use strict"
-	/**
-	 * EDD cart information in the header
-	 */
-	var cartTotalAmount = $j('.eddmenucart-details.total');
-
-	$j('body').on('edd_cart_item_added', function( event, response ) {
-
-		$j( '.edd-menu-icon' ).removeClass('edd-cart-empty');
-
-		cartTotalAmount.html( response.total );
-
-	});
-
-	$j('body').on('edd_cart_item_removed', function( event, response ) {
-
-		cartTotalAmount.html( response.total );
-	});
-
->>>>>>> 6238349295930c9be2289cb5c6fbcdab354fd6e2
 }  
