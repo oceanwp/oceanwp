37e77829ec212749ea92d35b448f21e2a098d15f
Nicolas <oceanwptheme@gmail.com>
2018-05-19 01:11:10 +0200

    Classes and pagination issue fixed

--- index.php
+++ index.php
@@ -33,58 +33,39 @@ get_header(); ?>
 				if ( have_posts() ) :
 
 					// Elementor `archive` location
-					if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'archive' ) ) {
+					if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'archive' ) ) { ?>
 
-						if( is_post_type_archive( 'download' ) || is_tax( array( 'download_category', 'download_tag' ) ) ) {
-							
-							do_action( 'ocean_before_archive_download' );
-							?>
-							<div class="oceanwp-row <?php echo esc_attr( oceanwp_edd_loop_classes() ); ?>">
-							
+						<div id="blog-entries" class="<?php oceanwp_blog_wrap_classes(); ?>">
+
 							<?php
-							// Archive Post Count for clearing float
-							$oceanwp_count = 0;
+							// Define counter for clearing floats
+							$oceanwp_count = 0; ?>
 
-							while ( have_posts() ) : the_post();
-
-								$oceanwp_count++;
-
-								get_template_part( 'partials/edd/archive' );
-
-								if ( oceanwp_edd_entry_columns() == $oceanwp_count ) {
-									$oceanwp_count=0;
-								}
-
-							endwhile;
-							?>
-							</div>
-							
 							<?php
-							do_action( 'ocean_after_archive_download' );
+							// Loop through posts
+							while ( have_posts() ) : the_post(); ?>
 
-						} else {
-							?>
-							<div id="blog-entries" class="entries clr oceanwp-row blog-grid">
-							<?php
+								<?php
+								// Add to counter
+								$oceanwp_count++; ?>
 
-							// Archive Post Count for clearing float
-							$oceanwp_count = 0;
+								<?php
+								// Get post entry content
+								get_template_part( 'partials/entry/layout', get_post_type() ); ?>
 
-							while ( have_posts() ) : the_post();
-
-								$oceanwp_count++;
-
-								get_template_part( 'partials/entry/layout', get_post_type() );
-
+								<?php
+								// Reset counter to clear floats
 								if ( oceanwp_blog_entry_columns() == $oceanwp_count ) {
-									$oceanwp_count = 0;
-								}
+									$oceanwp_count=0;
+								} ?>
 
-							endwhile;
-							?>
-							</div>
-							<?php
-						}
+							<?php endwhile; ?>
+
+						</div><!-- #blog-entries -->
+
+						<?php
+						// Display post pagination
+						oceanwp_blog_pagination();
 
 					} ?>
 
