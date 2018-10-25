a1c20f6f524b0ebbaf38e67f98740f7061630bf7
Nicolas <oceanwptheme@gmail.com>
2018-10-26 01:05:27 +0200

    Update inc/plugins/tgm-plugin-activation.php

--- inc/plugins/tgm-plugin-activation.php
+++ inc/plugins/tgm-plugin-activation.php
@@ -31,13 +31,6 @@ function oceanwp_tgmpa_register() {
 			'force_activation'	=> false,
 		),
 
-		array(
-			'name'				=> 'Auto Load Next Post',
-			'slug'				=> 'auto-load-next-post', 
-			'required'			=> false,
-			'force_activation'	=> false,
-		),
-
 	);
 
 	// Register notice
