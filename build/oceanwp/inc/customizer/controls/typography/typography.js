09a605485dc19e8c29dfd70d2ba49df2dc3d6ea8
Nicolas <oceanwptheme@gmail.com>
2018-10-28 13:13:00 +0100

    updated file

--- build/oceanwp/inc/customizer/controls/typography/typography.js
+++
@@ -1,9 +0,0 @@
-( function($) {
-
-	$( document ).ready(function () {
-
-		$( '.oceanwp-typography-select' ).select2();
-
-	} );
-
-} )( jQuery );
--- build/oceanwp/partials/edd/shortcode-content-cart-button.php
+++
@@ -1,3 +0,0 @@
-<div class="edd_download_buy_button">
-	<?php echo oceanwp_edd_add_to_cart_link(); ?>
-</div>
