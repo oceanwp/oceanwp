ef32eb531a025f301f59b9f64633a2e842edb3fe
Alex de Borba <AlexdeBorba@users.noreply.github.com>
2019-12-18 21:47:34 -0500

    Updated 4040.php

--- 404.php
+++ 404.php
@@ -21,7 +21,7 @@ if ( 'on' == get_theme_mod( 'ocean_error_page_blank', 'off' ) ) { ?>
 	<html <?php language_attributes(); ?><?php oceanwp_schema_markup( 'html' ); ?>>
 		<head>
 			<meta charset="<?php bloginfo( 'charset' ); ?>">
-			<link rel="profile" href="http://gmpg.org/xfn/11">
+			<link rel="profile" href="https://gmpg.org/xfn/11">
 
 			<?php wp_head(); ?>
 		</head>
