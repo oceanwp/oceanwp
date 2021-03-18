fe2c6fd5bc7fc4f319156a4dc13ff48bf4209ba1
Mohammad Taghi Elmi <mohammad.taghi.elmi@gmail.com>
2021-03-18 15:27:05 +0330

    Write Search Header Replace in Search Class

--- assets/src/js/components/search.js
+++ assets/src/js/components/search.js
@@ -10,6 +10,11 @@ export default class Search {
             DOM.search.dropdownToggleIcon.addEventListener("click", this.#onDropdownToggleIcon);
         }
 
+        if (options.menuSearchStyle == "header_replace") {
+            DOM.search.headerReplaceToggleIcon.addEventListener("click", this.#onHeaderReplaceToggleIcon);
+            DOM.search.headerReplaceCloseIcon.addEventListener("click", this.#onHeaderReplaceCloseIcon);
+        }
+
         window.addEventListener("click", this.#onWindowClick);
     };
 
@@ -22,22 +27,74 @@ export default class Search {
         form.classList.toggle("show");
         toggleIcon.parentNode.classList.toggle("active");
 
-        // Focus
-        let formTransitionDuration =
-            parseFloat(getComputedStyle(form).transitionDuration.replace("s", "")) * 1000;
+        this.#focusForm(form, "input.field");
+    };
 
-        if (formTransitionDuration) {
-            setTimeout(function () {
-                form.querySelector("input.field").focus();
-            }, formTransitionDuration);
+    #onHeaderReplaceToggleIcon = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const { headerReplaceForm: form } = DOM.search;
+
+        form.classList.toggle("show");
+
+        if (this.#hasTopHeader()) {
+            DOM.headerTopLeftSide.classList.toggle("hide");
+            DOM.headerTopRighttSide.classList.toggle("hide");
+        } else {
+            DOM.nav.classList.toggle("hide");
+            form.style.maxWidth =
+                document.querySelector("#site-navigation > ul.dropdown-menu")?.offsetWidth + 60;
+        }
+
+        this.#focusForm(form, 'input[type="search"]');
+    };
+
+    #onHeaderReplaceCloseIcon = (event) => {
+        event.preventDefault();
+
+        const { headerReplaceForm: form } = DOM.search;
+
+        form.classList.remove("show");
+
+        if (this.#hasTopHeader()) {
+            DOM.headerTopLeftSide.classList.remove("hide");
+            DOM.headerTopRighttSide.classList.remove("hide");
+        } else {
+            DOM.nav.classList.remove("hide");
         }
     };
 
     #onWindowClick = (event) => {
         // Dropdonw Search Style Collaps
         if (!event.target.closest("#searchform-dropdown.show")) {
-            DOM.search.dropdownForm.classList.remove("show");
-            DOM.search.dropdownToggleIcon.parentNode.classList.remove("active");
+            DOM.search.dropdownForm?.classList.remove("show");
+            DOM.search.dropdownToggleIcon?.parentNode.classList.remove("active");
+        }
+
+        // Header Replace Search Style Collaps
+        if (!event.target.closest("#searchform-header-replace.show")) {
+            DOM.search.headerReplaceForm.classList.remove("show");
+
+            if (this.#hasTopHeader()) {
+                DOM.headerTopLeftSide.classList.remove("hide");
+                DOM.headerTopRighttSide.classList.remove("hide");
+            } else {
+                DOM.nav.classList.remove("hide");
+            }
         }
     };
+
+    #focusForm = (form, inputClass) => {
+        const formTransitionDuration =
+            parseFloat(getComputedStyle(form).transitionDuration.replace("s", "")) * 1000;
+
+        if (formTransitionDuration) {
+            setTimeout(function () {
+                form.querySelector(inputClass).focus();
+            }, formTransitionDuration);
+        }
+    };
+
+    #hasTopHeader = () => DOM.header.classList.contains("top-header");
 }
--- assets/src/js/constants.js
+++ assets/src/js/constants.js
@@ -2,6 +2,10 @@ export const options = oceanwpLocalize;
 
 export const DOM = {
     body: document.body,
+    header: document.querySelector("#site-header"),
+    headerTopLeftSide: document.querySelector("#site-header.top-header .header-top .left"),
+    headerTopRightSide: document.querySelector("#site-header.top-header .header-top .right"),
+    nav: document.querySelector("#site-header.header-replace #site-navigation"),
     selects: document.querySelectorAll(options.customSelects),
     mobileMenu: {
         nav: document.querySelector("#mobile-dropdown"),
@@ -10,8 +14,12 @@ export const DOM = {
         menuItemsHasChildren: document.querySelectorAll("#mobile-dropdown .menu-item-has-children"),
     },
     search: {
+        forms: document.querySelectorAll("form.header-searchform"),
         dropdownToggleIcon: document.querySelector("a.search-dropdown-toggle"),
         dropdownForm: document.querySelector("#searchform-dropdown"),
+        headerReplaceToggleIcon: document.querySelector("a.search-header-replace-toggle"),
+        headerReplaceCloseIcon: document.querySelector("#searchform-header-replace-close"),
+        headerReplaceForm: document.querySelector("#searchform-header-replace"),
     },
 };
 
