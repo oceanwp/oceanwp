887b6d8ef2c174a14a0a1e80d7f8d07bad9a6a23
Mohammad Taghi Elmi <mohammad.taghi.elmi@gmail.com>
2021-04-12 11:37:15 +0430

    remove fitvids plugin

--- assets/js/core/infiniteScroll.js
+++ assets/js/core/infiniteScroll.js
@@ -1,81 +1,76 @@
-var $j 		= jQuery.noConflict(),
-	$window = $j( window );
+var $j = jQuery.noConflict(),
+    $window = $j(window);
 
-$window.on( 'load', function() {
-	"use strict";
-	if ( $j.fn.infiniteScroll !== undefined && $j( 'div.infinite-scroll-nav' ).length ) {
-		// Infinite scroll
-		oceanwpInfiniteScrollInit();
-	}
-} );
+$window.on("load", function () {
+    "use strict";
+    if ($j.fn.infiniteScroll !== undefined && $j("div.infinite-scroll-nav").length) {
+        // Infinite scroll
+        oceanwpInfiniteScrollInit();
+    }
+});
 
 /* ==============================================
 INFINITE SCROLL
 ============================================== */
 function oceanwpInfiniteScrollInit() {
-	"use strict"
+    "use strict";
 
-	// Get infinite scroll container
-	var $container = $j( '.infinite-scroll-wrap' );
+    // Get infinite scroll container
+    var $container = $j(".infinite-scroll-wrap");
 
-	// Start infinite sccroll
-	$container.infiniteScroll( {
-		path 	: '.older-posts a',
-		append 	: '.item-entry',
-		status 	: '.scroller-status',
-		hideNav : '.infinite-scroll-nav',
-		history : false,
-	} );
+    // Start infinite sccroll
+    $container.infiniteScroll({
+        path: ".older-posts a",
+        append: ".item-entry",
+        status: ".scroller-status",
+        hideNav: ".infinite-scroll-nav",
+        history: false,
+    });
 
-	$container.on( 'load.infiniteScroll', function( event, response, path, items ) {
+    $container.on("load.infiniteScroll", function (event, response, path, items) {
+        var $items = $j(response).find(".item-entry");
 
-		var $items = $j( response ).find( '.item-entry' );
+        $items.imagesLoaded(function () {
+            // Animate new Items
+            $items.animate({
+                opacity: 1,
+            });
 
-		$items.imagesLoaded( function() {
+            // Force the images to be parsed to fix Safari issue
+            $items.find("img").each(function (index, img) {
+                img.outerHTML = img.outerHTML;
+            });
 
-			// Animate new Items
-			$items.animate( {
-				opacity : 1
-			} );
+            // Isotope
+            if ($container.hasClass("blog-masonry-grid")) {
+                $container.isotope("appended", $items);
+                $items.css("opacity", 0);
+            }
 
-			// Force the images to be parsed to fix Safari issue
-			$items.find( 'img' ).each( function( index, img ) {
-				img.outerHTML = img.outerHTML;
-			} );
+            // Re-run functions
+            if (!$j("body").hasClass("no-carousel")) {
+                oceanwpInitCarousel($items);
+            }
 
-			// Isotope
-			if ( $container.hasClass( 'blog-masonry-grid' ) ) {
-				$container.isotope( 'appended', $items );
-				$items.css( 'opacity', 0 );
-			}
+            if (!$j("body").hasClass("no-lightbox")) {
+                oceanwpInitLightbox($items);
+            }
 
-			// Re-run functions
-			if ( ! $j( 'body' ).hasClass( 'no-carousel' ) ) {
-				oceanwpInitCarousel( $items );
-			}
+            if (!$j("body").hasClass("no-fitvids")) {
+                oceanwpInitFitVids($items);
+            }
 
-			if ( ! $j( 'body' ).hasClass( 'no-lightbox' ) ) {
-				oceanwpInitLightbox( $items );
-			}
+            // Match heights
+            if (!$j("body").hasClass("no-matchheight")) {
+                $j(".blog-equal-heights .blog-entry-inner").matchHeight({ property: "min-height" });
+            }
 
-			if ( ! $j( 'body' ).hasClass( 'no-fitvids' ) ) {
-				oceanwpInitFitVids( $items );
-			}
-
-			// Match heights
-			if ( ! $j( 'body' ).hasClass( 'no-matchheight' ) ) {
-				$j( '.blog-equal-heights .blog-entry-inner' ).matchHeight({ property: 'min-height' });
-			}
-
-		    // Gallery posts
-		    if ( $j( '.gallery-format' ).parent( '.thumbnail' ) && $j( '.blog-masonry-grid' ).length ) {
-				setTimeout( function() {
-					$j( '.blog-masonry-grid' ).isotope( 'layout' );
-				}, 600 + 1 );
-			}
-
-		} );
-
-	} );
-
+            // Gallery posts
+            if ($j(".gallery-format").parent(".thumbnail") && $j(".blog-masonry-grid").length) {
+                setTimeout(function () {
+                    $j(".blog-masonry-grid").isotope("layout");
+                }, 600 + 1);
+            }
+        });
+    });
 }
