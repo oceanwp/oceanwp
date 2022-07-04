70a87543083d6d00484fa612a2683db5d860ceb0
web-devise <manager@devise.com.ua>
2022-07-04 13:25:20 +0300

    Elementor Google Fonts control condition

--- inc/customizer/settings/typography.php
+++ inc/customizer/settings/typography.php
@@ -372,28 +372,30 @@ if ( ! class_exists( 'OceanWP_Typography_Customizer' ) ) :
 				)
 			);
 
-			$wp_customize->add_setting(
-				'ocean_local_elementor_google_font',
-				array(
-					'transport'         => 'postMessage',
-					'default'           => false,
-					'sanitize_callback' => 'oceanwp_sanitize_checkbox',
-				)
-			);
+			if ( did_action( 'elementor/loaded' ) ) {
+					$wp_customize->add_setting(
+						'ocean_local_elementor_google_font',
+						array(
+							'transport'         => 'postMessage',
+							'default'           => false,
+							'sanitize_callback' => 'oceanwp_sanitize_checkbox',
+						)
+					);
 
-			$wp_customize->add_control(
-				new WP_Customize_Control(
-					$wp_customize,
-					'ocean_local_elementor_google_font',
-					array(
-						'label'    => esc_html__( 'Elementor Google Fonts from Site', 'oceanwp' ),
-						'type'     => 'checkbox',
-						'section'  => 'ocean_typography_general',
-						'settings' => 'ocean_local_elementor_google_font',
-						'priority' => 10,
-					)
-				)
-			);
+					$wp_customize->add_control(
+						new WP_Customize_Control(
+							$wp_customize,
+							'ocean_local_elementor_google_font',
+							array(
+								'label'    => esc_html__( 'Elementor Google Fonts from Site', 'oceanwp' ),
+								'type'     => 'checkbox',
+								'section'  => 'ocean_typography_general',
+								'settings' => 'ocean_local_elementor_google_font',
+								'priority' => 10,
+							)
+						)
+					);
+				}
 
 			/**
 			 * Font Subsets
