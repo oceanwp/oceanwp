880bab7d66f9e8cba6738001c9ea7b1276c3ef3f
Amit Singh <apprimit@gmail.com>
2022-12-26 15:00:47 +0530

    language pot file regenerated

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,6 @@ module.exports = function (grunt) {
 
   // Load all Grunt tasks
   require("jit-grunt")(grunt, {
-    makepot: "grunt-wp-i18n",
     browserify: "grunt-browserify",
   });
 
@@ -456,41 +455,6 @@ module.exports = function (grunt) {
     clean: {
       build: ["build/<%= pkg.name %>", "build/<%= pkg.name %>.zip"],
     },
-
-    makepot: {
-      target: {
-        options: {
-          domainPath: "/languages/", // Where to save the POT file.
-          exclude: [
-            // Exlude folder.
-            "build/.*",
-            "vendor/.*",
-            "assets/.*",
-            "readme/.*",
-            "sass/.*",
-            "bower_components/.*",
-            "node_modules/.*",
-          ],
-          potFilename: "<%= pkg.name %>.pot", // Name of the POT file.
-          type: "wp-theme", // Type of project (wp-plugin or wp-theme).
-          updateTimestamp: true, // Whether the POT-Creation-Date should be updated without other changes.
-          processPot: function (pot, options) {
-            pot.headers["plural-forms"] = "nplurals=2; plural=n != 1;";
-            pot.headers["last-translator"] = "OceanWP\n";
-            pot.headers["language-team"] = "OceanWP\n";
-            pot.headers["x-poedit-basepath"] = "..\n";
-            pot.headers["x-poedit-language"] = "English\n";
-            pot.headers["x-poedit-country"] = "UNITED STATES\n";
-            pot.headers["x-poedit-sourcecharset"] = "utf-8\n";
-            pot.headers["x-poedit-searchpath-0"] = ".\n";
-            pot.headers["x-poedit-keywordslist"] =
-              "_esc_attr__;esc_attr_x;esc_attr_e;esc_html__;esc_html_e;esc_html_x;__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c;_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;\n";
-            pot.headers["x-textdomain-support"] = "yes\n";
-            return pot;
-          },
-        },
-      },
-    },
   });
 
   // Dev task
@@ -503,7 +467,6 @@ module.exports = function (grunt) {
     "sass:dev",
     "autoprefixer:main",
     "cssmin:prod",
-    "makepot",
   ]);
 
   // Production task
