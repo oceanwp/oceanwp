378aca22d7ee308637981abd895444f5483781c4
Amit Singh <apprimit@gmail.com>
2023-06-14 11:13:10 +0530

    update woo template

--- woocommerce/cart/mini-cart.php
+++ woocommerce/cart/mini-cart.php
@@ -13,8 +13,8 @@
  * the readme will list any important changes.
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates
- * @version 5.2.0
+ * @package WooCommerce\Templates
+ * @version 7.8.0
  */
 
 defined( 'ABSPATH' ) || exit;
