00f79248b6bf298539e1e2978084c269802719b6
Amit Singh <apprimit@gmail.com>
2023-12-28 11:09:43 +0530

    added close button for modals

--- inc/customizer/setup/assets/dist/index.asset.php
+++ inc/customizer/setup/assets/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f395bb7d08883ffe633f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '061e87d9b6aeb207496f');
