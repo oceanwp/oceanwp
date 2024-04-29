a4ba21430676de847a00e43572acf55b4795402f
Amit Singh <apprimit@gmail.com>
2024-04-29 13:56:13 +0530

    oec off canvas widget + scroll effect

--- assets/src/js/theme/scroll-effect.js
+++ assets/src/js/theme/scroll-effect.js
@@ -62,7 +62,9 @@ class ScrollEffect {
       !scrollItem.classList.contains("opl-link") &&
       !scrollItem.parentNode.classList.contains("opl-link") &&
       !scrollItem.classList.contains("oew-off-canvas-button") &&
-      !scrollItem.parentNode.classList.contains("oew-off-canvas-button")
+      !scrollItem.parentNode.classList.contains("oew-off-canvas-button") &&
+      !scrollItem.classList.contains("oec-off-canvas-button") &&
+      !scrollItem.parentNode.classList.contains("oec-off-canvas-button")
     ) {
       const href = scrollItem.getAttribute("href");
       const id = href.substring(href.indexOf("#")).slice(1);
