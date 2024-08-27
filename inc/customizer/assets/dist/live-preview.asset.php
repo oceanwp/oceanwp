f5c4483adaa0f9e9799b35e920f58cada26a4778
Amit Singh <apprimit@gmail.com>
2024-08-27 13:04:40 +0530

    style issue

--- inc/customizer/assets/dist/live-preview.asset.php
+++ inc/customizer/assets/dist/live-preview.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '0fc067e632187d6db657');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'eb651b0d03c80749f637');
