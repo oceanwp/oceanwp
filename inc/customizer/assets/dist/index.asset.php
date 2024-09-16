ba45784739e6862db5515bd935e8dfbb60fbda78
Amit Singh <apprimit@gmail.com>
2024-09-16 11:17:00 +0530

    top and bottom margin for controls

--- inc/customizer/assets/dist/index.asset.php
+++ inc/customizer/assets/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => 'c21c7140f9867517e63e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => 'e9d64b5034da53a261a2');
