a0d7f8a51c95ba4a3be5ccead63d016c6d085589
Amit Singh <apprimit@gmail.com>
2026-08-11 11:45:52 +0530

    some fixes (16719)

--- inc/customizer/assets/dist/index.asset.php
+++ inc/customizer/assets/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => 'b66461d1d2ffd8afa116');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => 'a8d403644b15ad5af466');
