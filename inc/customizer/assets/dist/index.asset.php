c7919eceb7f6b1df9296caba585ffc52d7a546b9
Amit Singh <apprimit@gmail.com>
2026-08-12 16:55:42 +0530

    woo section priority (16803)

--- inc/customizer/assets/dist/index.asset.php
+++ inc/customizer/assets/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => '8101b9da419860ded8c5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-primitives'), 'version' => 'fe8d71bae512b57cef74');
